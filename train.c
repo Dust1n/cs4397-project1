225eae5ac88a89a145dc145ea97a1a472c0ec7f8
Nick Miller <mr.nick.miller@gmail.com>
2014-09-07 21:24:14 -0500

    Added `main`

--- train.c
+++ train.c
@@ -4,46 +4,58 @@
 #include <msgQLib.h>
 #include <stdio.h>
 
+// Global data structures
+#define ENGINE_ADDRESS 1
+
 unsigned char SerBuf[3], x;
 int ser_fd;
 int dir;
 
 // Function declarations
-void SendCommand();
-void blowTrainHorn();
+void init(void);
+void sendCommand(void);
+void blowHorn(void);
 
-// Global data structures
-#define ENGINE_ADDRESS 1
+int main(int argc, const char* argv[])
+{
+	printf("CS 4397.001 Project 1: Trainmaster Train System\n\n");
+	printf("Select from the menu below:\n");
+	printf("A. Ring the train's bell\n");
+	printf("B. Start the train's engine\n");
+	printf("C. Stop the train's engine\n");
+	printf("D. Exit");
+	
+	// TODO: Get and process user input
+}
 
+// Initializes the serial connection.
 void init(void)
 {
 	// Open serial port
 	ser_fd = open("/tyCo/1", O_WRONLY, 0);
-	printf("\n train id %d", ENGINE_ADDRESS);
+	printf("\nTrain ID: %d", ENGINE_ADDRESS);
 	
 	if (ser_fd == -1)
 	{
-		printf("\nSerial Port Not Open, error\n", ser_fd, 0, 0, 0, 0, 0);
+		printf("[ERROR] Unable to open serial port.\n");
 	}
 	else
 	{
-		printf("\nSerial Port Open, fd = %d \n", ser_fd, 0, 0, 0, 0, 0);
+		printf("Successfully opened serial port; fd = %d\n", ser_fd);
 	}
 }
 
-// TODO: Add main()
-
-// Sends data to the train via the serial connection
-void SendCommand()
+// Sends data to the train via the serial connection.
+void sendCommand(void)
 {
 	int numByteWritten = 0;
 	SerBuf[0] = (unsigned char)254;
 	SerBuf[1] = (unsigned char)0;
 	numByteWritten = write(ser_fd, SerBuf, 3);
-} 
+}
 
-// Instructs the train to blow its horn
-void blowTrainHorn()
+// Instructs the train to blow its horn.
+void blowHorn(void)
 {
 	SerBuf[2] = 0x9c;
 
